5d7d36c75b837a97c9ea7ad1d03b1c181e12cc49
adamkosovic <adam.kosovic@icloud.com>
2025-07-18 02:39:55 +0200

    Fix User model Books property initialization

--- Models/User.cs
+++ Models/User.cs
@@ -4,7 +4,7 @@ namespace TestPraktik.Models;
 
 public class User : IdentityUser
 {
-  public List<Book> Books { get; set; }
+  public List<Book> Books { get; set; } = new List<Book>();
 
   public User() { }
 }
