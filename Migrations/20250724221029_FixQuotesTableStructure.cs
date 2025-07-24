f48fff94889ce4bae2a53d6bd3b2c719df346209
adamkosovic <adam.kosovic@icloud.com>
2025-07-25 00:10:32 +0200

    Add migration to fix quotes table structure

---
+++ Migrations/20250724221029_FixQuotesTableStructure.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestPraktik.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixQuotesTableStructure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Quotes_AspNetUsers_UserId",
+                table: "Quotes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Quotes",
+                table: "Quotes");
+
+            migrationBuilder.RenameTable(
+                name: "Quotes",
+                newName: "quotes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Quotes_UserId",
+                table: "quotes",
+                newName: "IX_quotes_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_quotes",
+                table: "quotes",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_quotes_AspNetUsers_UserId",
+                table: "quotes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_quotes_AspNetUsers_UserId",
+                table: "quotes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_quotes",
+                table: "quotes");
+
+            migrationBuilder.RenameTable(
+                name: "quotes",
+                newName: "Quotes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_quotes_UserId",
+                table: "Quotes",
+                newName: "IX_Quotes_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Quotes",
+                table: "Quotes",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Quotes_AspNetUsers_UserId",
+                table: "Quotes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
